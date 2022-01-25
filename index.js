7782314e4a78e2b5fa6affad874c2b37096c5516
Gregory Josephs <greg.josephs@gmail.com>
2022-01-25 13:16:33 -0500

    Added functionality to add items to list

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function App(){
+  {/*
+  Here to begin weith, we are setting our initial todos and their initial state */}
   const [todos, setTodos] = React.useState([
     {
       text: 'learn react',
@@ -19,12 +21,33 @@ function App(){
     {
       text: 'stain the beams in the great room',
       isCompleted: false,
+    },
+    {
+      text: 'feed the sourdough starter',
+      isCompleted: false
     }       
-  ])
+  ]);
 
+  const [value, setValue] = React.useState('');
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    const newTodos = [...todos, {text:value, isCompleted:false}];
+    setTodos(newTodos);
+    setValue('');
+  }
   return(
     <>
       {todos.map((todo, i) => <div className="todo" key={i}>{todo.text}</div>)}
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text"
+          className="input"
+          value={value}
+          placeholder="Add Todo ..."
+          onChange={e => setValue(e.target.value)}
+          />
+      </form>
     </>
   );
 }
