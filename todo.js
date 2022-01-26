659d7489aee78de7384b686c0be6395d579b3047
Gregory Josephs <greg.josephs@gmail.com>
2022-01-26 05:40:48 -0500

    refactored according to lesson

--- todo.js
+++ todo.js
@@ -1,40 +1,8 @@
-function ToDo(){
-    const [todos, setTodos] = React.useState([
-        {
-          text: 'learn react',
-          isCompleted: false,
-        },
-        {
-          text: 'meet friend for lunch',
-          isCompleted: false,
-        },
-        {
-          text: 'build todo app',
-          isCompleted: false,
-        }, 
-        {
-          text: 'finish mudding the dining room',
-          isCompleted: false,
-        },
-        {
-          text: 'stain the beams in the great room',
-          isCompleted: false,
-        },
-        {
-          text: 'feed the sourdough starter',
-          isCompleted: false
-        }       
-      ]);
-    
-      
-      
-      const removeTodo = e => {
-        const index = Number(e.target.id);
-        let temp = [...todos];
-        temp.splice(index, 1);
-        setTodos(temp);
-      };
-      return (
-        todos.map((todo, i) => <div className="todo" key={i} id={i} onClick={removeTodo}>{todo.text}</div>)
-      )
+function ToDo({todo, index, remove}){
+    function handle(){
+        remove(index);
+    }  
+    return (
+        <div className="todo" onClick={handle}>{todo.text} (-)</div>
+    )
 }
